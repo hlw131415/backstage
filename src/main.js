b97e49c0d5eb0d5ef807ef2ed4c93926f7f08dce
黄龙伟 <longwei.huang@waichi.com>
2022-05-20 17:19:57 +0800

    update

--- src/main.js
+++ src/main.js
@@ -10,6 +10,12 @@ import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 
+// 根据环境变量决定是否引入mock
+if (process.env.NODE_ENV !== 'production' && !process.env.VUE_APP_BACK_END_URL) {
+  const Mock = require('./mock/index').default
+  Mock.mockData()
+}
+
 new Vue({
   router,
   store,
